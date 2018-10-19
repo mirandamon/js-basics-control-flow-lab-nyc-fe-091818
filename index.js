6c3b47ed081e3d13ddf02b45010ae827d43931c6
Nathan Miranda <nate845@gmail.com>
2018-10-19 00:44:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ function switchOnCharmFromTip(tip) {
   switch(tip) {
     case 'generous':
       return 'Thank you so much.'
+      break
     case 'not as generous':
       return 'Thank you.'
     case 'thanks for everything':
