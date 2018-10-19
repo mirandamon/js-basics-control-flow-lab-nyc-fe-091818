6efe49404eb45f28f3570c991f2d20296ae87f82
Nathan Miranda <nate845@gmail.com>
2018-10-19 00:31:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function scuberGreetingForFeet(numberOfFeet) {
   if (numberOfFeet <= 400) {
     return 'This one is on me!'
   } else if (numberOfFeet > 2000) {
-    return 
+    return 'I will gladly take your thirty bucks.'
   }
 }
